9b5f9dd6c4ed21c1b0f693902bcfc487ee62b528
Wojciech Balon <balonw@gmail.com>
2023-10-04 12:53:37 +0200

    added sorting feature and simple.css styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     if current_user
       github_service = GithubService.new(current_user.github_access_token)
       @repositories = github_service.user_repositories_with_star_count
+      @repositories = @repositories.sort_by { |repo| repo['updated_at'] }.reverse
     else
       redirect_to login_path, alert: 'Please log in to view your dashboard.'
     end
