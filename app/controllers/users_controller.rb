bfc5781ca44952235d530e6caa6d360be8c968ed
Wojciech Balon <balonw@gmail.com>
2023-11-02 14:41:42 +0100

    extracted the logic from the controller into the repository service object.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,8 @@ class UsersController < ApplicationController
   def dashboard
     return redirect_to login_path, alert: 'Please log in to view your dashboard.' unless current_user
 
-    github_service = GithubService.new(current_user.github_access_token)
-    all_repositories = github_service.user_repositories_with_star_count
-    @repo_names = all_repositories.map { |repo| repo['name'] }
-
-    @repositories = filter_and_sort_repositories(all_repositories, params[:query])
-  end
-
-  private
-
-  def filter_and_sort_repositories(repositories, query)
-    filtered_repos = if query.present?
-                       repositories.select { |repo| repo['name'].downcase.include?(query.downcase) }
-                     else
-                       repositories
-                     end
-
-    filtered_repos.sort_by { |repo| DateTime.parse(repo['updated_at'] || '') }.reverse
+    repository_service = RepositoryService.new(current_user)
+    @repositories = repository_service.fetch_repositories(params[:query])
+    @repo_names = @repositories.map { |repo| repo['name'] }
   end
 end
